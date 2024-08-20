614a7d8f76ffdae121c81ae926b7f5ce2cf94fcc
Rabia-Rajput <rajput.rabia@outlook.com>
2024-08-20 13:53:38 +0200

    update InfoBox file

--- src/Components/InfoBox.tsx
+++ src/Components/InfoBox.tsx
@@ -1,19 +1,25 @@
 import { ReactNode } from "react";
 
-type InfoBoxProps={
-    mode: 'hint'|'warning',
-    children:ReactNode;
-}
-
-const InfoBox = ({mode,children}:InfoBoxProps) => {
-  return (
-    <aside>
-        <h2>warning</h2>
-      <p>{children}</p>
-
-
-    </aside>
-  );
+type InfoBoxProps = {
+  mode: "hint" | "warning";
+  severity?:'low'|'medium'| 'high';
+  children: ReactNode;
 };
+
+const InfoBox = ({ mode, children,severity }: InfoBoxProps) => {
+  if (mode === "hint") {
+    return (
+      <aside className=" infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+    return (
+      <aside className={`infobox infobox-warning warning--${severity}`}>
+        <h2>Warning</h2>
+        <p>{children}</p>
+      </aside>
+    );
+  }
 
 export default InfoBox;
