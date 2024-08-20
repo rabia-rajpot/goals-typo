59fb9d152ffcec80f51dd2fa2dc5766faf6c5032
Rabia-Rajput <rajput.rabia@outlook.com>
2024-08-20 13:53:31 +0200

    updated coursegoalList file

--- src/Components/CourseGoalList.tsx
+++ src/Components/CourseGoalList.tsx
@@ -1,13 +1,31 @@
 import First from "./First";
 import { CourseGoal } from "../App";
- type CourseGoalListProps = {
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
+type CourseGoalListProps = {
   goals: CourseGoal[];
-  onDeleteGoal:(id:number)=>void;
- };
+  onDeleteGoal: (id: number) => void;
+};
 
-const CourseGoalList = ({ goals, onDeleteGoal}:CourseGoalListProps ) => {
+const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint" >
+        You have no Course Goals yet,start adding some!
+      </InfoBox>
+    );
+  }
+  let warningBox: ReactNode;
+  if (goals.length >= 5) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You are collecting a lot of goals.Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
     <div>
+        {warningBox}
       <ul>
         {goals.map((goal) => (
           <li key={goal.id}>
